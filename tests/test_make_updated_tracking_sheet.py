7ac79d541b157e8f17fa774c92598f7fcff2e01a
Ben Fulroth <fulrothb@icloud.com>
2020-08-24 08:25:16 -0400

    Testing: Commented out testing class placeholder

--- tests/test_make_updated_tracking_sheet.py
+++ tests/test_make_updated_tracking_sheet.py
@@ -82,24 +82,24 @@ class TestIOMethods(TestCase):
             self.assertIn(header, result.columns)
 
 
-class test_save(TestCase):
-    """Class for testing something"""
+# class test_save(TestCase):
+#     """Class for testing something"""
+#
+#     @classmethod
+#     def setUpClass(cls) -> None:
+#         """
+#         Method used to setup fixtures for testing
+#         :return:
+#         """
+#
+#         # Variable to be used in all tests in this class.
+#         cls.var_name = ''
+#         pass
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        """
-        Method used to setup fixtures for testing
-        :return:
-        """
-
-        # Variable to be used in all tests in this class.
-        cls.var_name = ''
-        pass
-
-    @patch('full.path.to.some.method', return_value='return_something')
-    def test_something(self, mock_1):
-        """
-        Test something...
-        :param mock_1: Mocks the 'fill this in' method
-        """
-        pass
+    # @patch('full.path.to.some.method', return_value='return_something')
+    # def test_something(self, mock_1):
+    #     """
+    #     Test something...
+    #     :param mock_1: Mocks the 'fill this in' method
+    #     """
+    #     pass
